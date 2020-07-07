ba7c704edd9eda7840fe0540b14d974e8083d5ac
Alexander Calvillo <alexanderjcalvillo@gmail.com>
2020-07-07 16:20:58 -0500

    adds post and get ajax to client with button click, and transfers data from server to database

--- database.sql
+++ database.sql
@@ -3,3 +3,10 @@ CREATE TABLE "artists" (
     "artist_name" varchar(80) not null,
     "year_born" date
 );
+
+CREATE TABLE "songs" (
+    "id" SERIAL PRIMARY KEY,
+    "title" varchar(80) NOT NULL,
+    "length" varchar(12) NOT NULL,
+    "date_released" date
+);
--- server/routes/song.router.js
+++ server/routes/song.router.js
@@ -1,28 +1,62 @@
 const express = require('express');
 const router = express.Router();
+const pg = require('pg');
 
+const Pool = pg.Pool;
+
+const pool = new Pool({
+  database: 'jazzy_ajax',
+  host: 'localhost',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000,
+});
 // static content. this will be replaced with a database table
-const songListArray = [
-    {
-        title: 'Take Five',
-        length: '2:55',
-        date_released: '1959-09-29'
-    },
-    {
-        title: 'So What',
-        length: '9:22',
-        date_released: '1959-08-17'
-    }
-];
+// const songListArray = [
+//   {
+//     title: 'Take Five',
+//     length: '2:55',
+//     date_released: '1959-09-29',
+//   },
+//   {
+//     title: 'So What',
+//     length: '9:22',
+//     date_released: '1959-08-17',
+//   },
+// ];
 
+// get info from the server
 router.get('/', (req, res) => {
-    console.log(`In /songs GET`);
-    res.send(songListArray);
+  console.log(`In /songs GET`);
+  const queryText = `SELECT * FROM "songs";`;
+  pool
+    .query(queryText)
+    .then((dbResponse) => {
+      console.log(dbResponse.rows);
+      res.send(dbResponse.rows);
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log("Something was suppose to come back, but it didn't.");
+      res.sendStatus(500);
+    });
 });
 
 router.post('/', (req, res) => {
-    
-    res.sendStatus(201);
+  const songs = req.body;
+  const queryText = `INSERT INTO "songs" ("title", "length", "date_released")
+VALUES ($1, $2, $3);`;
+  pool
+    .query(queryText, [songs.title, songs.length, songs.date_released])
+    .then((dbResponse) => {
+      console.log(dbResponse);
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log("I don't think that's it's chief");
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
